a97c31cdbca03c93ebedfbbaacf4013261a0b77d
m0nica1998 <monica.cesario98@icloud.com>
2025-03-29 16:40:06 +0100

    esercizio if, else e operatore ternario

--- src/main/main.java
+++ src/main/main.java
@@ -2,7 +2,29 @@ package main;
 
 public class main {
 	public static void main(String[] args) {
+		//creare una String messaggio valorizzata con “oggi è proprio”, e un int giornoDellaSettimana con un valore da 1 a 7 
 		
+		String messaggio= "oggi è proprio";
+		int giornoDellaSettimana = 4;
+		
+		//usare l’operatore ternario per concatenare al messaggio “un” o “una” in base al giorno della settimana 
+		String x = giornoDellaSettimana != 7 ? "un" : "una";
+		//usare if else if per concatenare il giorno della settimana, corrispondente al numero, al messaggio
+		if(giornoDellaSettimana == 1) {
+			System.out.println(messaggio + " " + x + " lunedì");
+		} else if(giornoDellaSettimana == 2) {
+			System.out.println(messaggio + " " + x + " martedì");
+		} else if(giornoDellaSettimana == 3) {
+			System.out.println(messaggio + " " + x + " mercoledì");
+		} else if(giornoDellaSettimana == 4) {
+			System.out.println(messaggio + " " + x + " giovedì");
+		} else if(giornoDellaSettimana == 5) {
+			System.out.println(messaggio + " " + x + " venerdì");
+		} else if(giornoDellaSettimana == 6) {
+			System.out.println(messaggio + " " + x + " sabato");
+		} else {
+			System.out.println(messaggio + " " + x + " domenica");
+		}
 	}
 
 }
